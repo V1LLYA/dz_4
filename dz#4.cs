58434e969a96b0714e836ffef4e4d618ccf0fd23
Aslanov Vilen <122909647+V1LLYA@users.noreply.github.com>
2023-06-04 17:20:43 +0300

    Add files via upload

---
+++ dz#4.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// for(int i = 0; i < 10; i++){
+//     Console.WriteLine("Введите число, которое будете возводить в степень: ");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите число, которое будете возводить в степень: ");
+//     int deg = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine($"Число {num} в степени {deg} ==> {Math.Pow(num, deg)}");
+// }
+
+// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// Console.WriteLine("Введите трехзначное число для подсчета суммы цифр в числе: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num >= 100 && num <= 999){
+//     int num1 = num % 10;
+//     int num2 = num / 10;
+//     int num3 = num2 % 10;
+//     int num4 = num2 / 10;
+//     Console.WriteLine($"Сумма цифр в числе {num} ==> {num1 + num3 + num4}");
+// } else Console.WriteLine("Число не трехзначное");
